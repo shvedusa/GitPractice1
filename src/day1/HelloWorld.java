3943447fa02cf01b7732bb97f15a980e6d73bfe8
Pavel Shvetsov <shvedusa@gmail.com>
2021-04-24 16:57:28 -0400

    last comment

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Adding GitHub Now");
-        System.out.println("Yoopy ay yoopy yay");
+        System.out.println("Yuppy ay yuppy yay");
+        System.out.println("Last comment for today");
     }
 }
