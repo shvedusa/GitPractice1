e378536c4c940e9f4d785a69cbdc07ef09baf0e1
Pavel Shvetsov <shvedusa@gmail.com>
2021-04-24 16:52:40 -0400

    Added more work

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -3,5 +3,7 @@ package day1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Adding GitHub Now");
+        System.out.println("Yoopy ay yoopy yay");
     }
 }
